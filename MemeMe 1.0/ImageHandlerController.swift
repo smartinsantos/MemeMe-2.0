bae2997df0a787aa9484dd100b25862f292438c5
Sergio <smartin88@gatech.edu>
2019-05-21 22:44:35 -0500

    saving progress

--- MemeMe 1.0/ImageHandlerController.swift
+++ MemeMe 1.0/ImageHandlerController.swift
@@ -9,62 +9,77 @@
 import UIKit
 
 class ImageHandlerController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     // MARK: ImageHandlerController Outlets
-    @IBOutlet weak var pickImage: UIBarButtonItem!
-    @IBOutlet weak var cameraButton: UIBarButtonItem!
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var navigationBarItem: UINavigationItem!
     @IBOutlet weak var topText: UITextField!
     @IBOutlet weak var bottomText: UITextField!
-    
-    
+    @IBOutlet weak var pickImage: UIBarButtonItem!
+    @IBOutlet weak var toolbar: UIToolbar!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
+    var shareButton: UIBarButtonItem!
+    var cancelButton: UIBarButtonItem!
+
     // MARK: ImageHandlerController Properties
     enum buttonTypes: Int { case photoLibrary = 1, camera }
-    
     struct Meme {
-        var topText: String = ""
-        var bottomText: String = ""
+        var topText: String
+        var bottomText: String
         var originalImage: UIImage
         var memedImage: UIImage
+        
+        init(topText: String, bottomText: String, originalImage: UIImage, memedImage: UIImage) {
+            self.topText = topText;
+            self.bottomText = bottomText;
+            self.originalImage = originalImage;
+            self.memedImage = memedImage;
+        }
     }
-    
     let textFieldsAttributes: [NSAttributedString.Key: Any] = [
         NSAttributedString.Key.strokeColor: UIColor.black,
         NSAttributedString.Key.strokeWidth: -4.5,
         NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
         NSAttributedString.Key.foregroundColor: UIColor.white
     ]
-    
     let textFieldsDelegate = TextFieldsDelegate();
-    
+        
     // MARK: ImageHandlerController Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        applyTextFieldProps()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
-        subscribeToKeyboardNotifications()
+        setupUI()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        
         super.viewWillDisappear(animated)
         unsubscribeFromKeyboardNotifications()
     }
     
     // MARK: ImageHandlerController Methods
-    func applyTextFieldProps() -> Void {
+    func setupUI() -> Void {
         // delegates
         self.topText.delegate = self.textFieldsDelegate
         self.bottomText.delegate = self.textFieldsDelegate
         // ui
         self.topText.defaultTextAttributes = textFieldsAttributes
         self.bottomText.defaultTextAttributes = textFieldsAttributes
+        topText.text = "TOP"
+        bottomText.text = "BOTTOM"
+        
+        shareButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.action, target: self, action: #selector(self.shareMeme))
+        shareButton.isEnabled = false;
+        cancelButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.cancel, target: self, action: #selector(eraseImage))
+        navigationBarItem.setLeftBarButtonItems([shareButton], animated: true)
+        navigationBarItem.setRightBarButtonItems([cancelButton], animated: true)
+        
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        subscribeToKeyboardNotifications()
     }
     
-
     func subscribeToKeyboardNotifications() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
@@ -90,6 +105,16 @@ class ImageHandlerController: UIViewController, UIImagePickerControllerDelegate,
         view.frame.origin.y += getKeyboardHeight(notification)
     }
     
+    func generateMemedImage() -> UIImage {
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return memedImage
+    }
+    
     // MARK: ImageHandlerController UIImagePickerControllerDelegate Methods
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
@@ -99,9 +124,7 @@ class ImageHandlerController: UIViewController, UIImagePickerControllerDelegate,
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = image
         }
-        
-        topText.text = "TOP"
-        bottomText.text = "BOTTOM"
+        self.shareButton.isEnabled = true
         dismiss(animated: true, completion: nil)
     }
     
@@ -122,6 +145,29 @@ class ImageHandlerController: UIViewController, UIImagePickerControllerDelegate,
         }
         
         present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    @objc func shareMeme() {
+        toolbar.isHidden = true
+        // I don't quite understand why we need this strucuture unless this is later going to be saved in local storage
+//        let meme = Meme(
+//            topText: topText.text!,
+//            bottomText: bottomText.text!,
+//            originalImage: imageView.image!,
+//            memedImage: generateMemedImage()
+//        )
+        let memedImage = generateMemedImage()
+        toolbar.isHidden = false
+        
+        let vc = UIActivityViewController(activityItems: [memedImage], applicationActivities: [])
+        present(vc, animated: true)
+    }
+    
+    @objc func eraseImage() {
+        topText.text = "TOP"
+        bottomText.text = "BOTTOM"
+        imageView.image = nil
+        shareButton.isEnabled = false
     }
 }
 
